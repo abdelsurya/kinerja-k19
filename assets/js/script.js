8762fe3520b702f994e551db25d9484d18065e8c
abdelsurya <abdelsurya@gmail.com>
2023-07-02 22:52:24 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,172 @@
+//line
+var kpiJuni = document.getElementById("kpijuni");
+new Chart("kpijuni", {
+  type: "line",
+  data: {
+    labels: ["21-06", "22-06", "23-06", "24-06", "25-06", "26-06", "27-06", "29-06", "30-06"],
+    datasets: [
+      {
+        label: "Realisasi",
+        data: [70.58, 71.05, 71.25, 71.44, 71.3, 71.9, 72.58, 73.24, 73.23],
+        backgroundColor: ["rgba(0, 137, 132, .2)"],
+        borderColor: ["rgba(0, 10, 130, .7)"],
+        borderWidth: 2,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    title: {
+      display: true,
+      text: "Konsolidasi Harian",
+    },
+  },
+});
+
+var kpiJuli = document.getElementById("kpijuli");
+new Chart("kpijuli", {
+  type: "line",
+  data: {
+    labels: ["01-07", "02-07", "03-07", "04-07", "05-07", "06-07", "07-07", "08-07", "09-07", "10-07"],
+    datasets: [
+      {
+        label: "Realisasi",
+        data: [57.04],
+        backgroundColor: ["rgba(0, 137, 132, .2)"],
+        borderColor: ["rgba(0, 10, 130, .7)"],
+        borderWidth: 2,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    title: {
+      display: true,
+      text: "Konsolidasi Harian",
+    },
+  },
+});
+
+var aktifPu = document.getElementById("aktifpu");
+new Chart("aktifpu", {
+  type: "line",
+  data: {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "Target",
+        data: [227503, 227503, 227503, 227503, 227503, 227503, 227503],
+        backgroundColor: ["rgba(105, 0, 132, .2)"],
+        borderColor: ["rgba(200, 99, 132, .7)"],
+        borderWidth: 2,
+      },
+      {
+        label: "Realisasi",
+        data: [197055, 198563, 199384, 200635, 200234, 201767],
+        backgroundColor: ["rgba(0, 137, 132, .2)"],
+        borderColor: ["rgba(0, 10, 130, .7)"],
+        borderWidth: 2,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    title: {
+      display: true,
+      text: "TK Aktif PU",
+    },
+  },
+});
+
+var aktifBpu = document.getElementById("aktifbpu");
+new Chart("aktifbpu", {
+  type: "line",
+  data: {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "Target",
+        data: [187199, 187199, 187199, 187199, 187199, 187199, 187199],
+        backgroundColor: ["rgba(105, 0, 132, .2)"],
+        borderColor: ["rgba(200, 99, 132, .7)"],
+        borderWidth: 2,
+      },
+      {
+        label: "Realisasi",
+        data: [140526, 144931, 148916, 151141, 149866, 151818],
+        backgroundColor: ["rgba(0, 137, 132, .2)"],
+        borderColor: ["rgba(0, 10, 130, .7)"],
+        borderWidth: 2,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    title: {
+      display: true,
+      text: "TK Aktif BPU",
+    },
+  },
+});
+
+var iuranPu = document.getElementById("iuranpu");
+new Chart("iuranpu", {
+  type: "line",
+  data: {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "Target",
+        data: [83.3, 166.6, 250, 333.3, 416.7, 500],
+        backgroundColor: ["rgba(105, 0, 132, .2)"],
+        borderColor: ["rgba(200, 99, 132, .7)"],
+        borderWidth: 2,
+      },
+      {
+        label: "Realisasi",
+        data: [66.6, 148.9, 234, 315, 405.8, 490.1],
+        backgroundColor: ["rgba(0, 137, 132, .2)"],
+        borderColor: ["rgba(0, 10, 130, .7)"],
+        borderWidth: 2,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    title: {
+      display: true,
+      text: "Iuran PU",
+    },
+  },
+});
+
+var iuranBpu = document.getElementById("iuranbpu");
+new Chart("iuranbpu", {
+  type: "line",
+  data: {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "Target",
+        data: [1.1, 2.3, 3.5, 4.6, 5.8, 7.0],
+        backgroundColor: ["rgba(105, 0, 132, .2)"],
+        borderColor: ["rgba(200, 99, 132, .7)"],
+        borderWidth: 2,
+      },
+      {
+        label: "Realisasi",
+        data: [1.3, 2.5, 4.0, 5.3, 6.1, 7.1],
+        backgroundColor: ["rgba(0, 137, 132, .2)"],
+        borderColor: ["rgba(0, 10, 130, .7)"],
+        borderWidth: 2,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    title: {
+      display: true,
+      text: "Iuran BPU",
+    },
+  },
+});
