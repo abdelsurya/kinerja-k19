e37783063e19d7d55e2f01b79d5b15efa0939823
abdelsurya <abdelsurya@gmail.com>
2023-07-08 02:13:34 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,35 @@
-//line
-const date = new Date(document.lastModified).toISOString().substr(11, 8);
-document.querySelector(".update").innerHTML = date;
+var date = new Date();
+var finaltime = timeSince(date);
+setInterval(function () {
+  document.getElementsByClassName("update")[0].innerHTML = timeSince(date);
+}, 30000);
+document.getElementsByClassName("update")[0].innerHTML = timeSince(date);
+
+function timeSince(date) {
+  var seconds = Math.floor((new Date() - date) / 1000);
+  var interval = Math.floor(seconds / 31536000);
+
+  if (interval > 1) {
+    return interval + " years";
+  }
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) {
+    return interval + " months";
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) {
+    return interval + " days";
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) {
+    return interval + " hours";
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval >= 1) {
+    return interval + " minutes";
+  }
+  return Math.floor(seconds) + " seconds";
+}
 
 var kpiJuni = document.getElementById("kpijuni");
 new Chart("kpijuni", {
