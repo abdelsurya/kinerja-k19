53ecf4b7c1fa187c3c262364cf01e8e0978f3a45
abdelsurya <abdelsurya@gmail.com>
2023-07-08 05:33:22 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,59 @@
-var date = new Date();
-var finaltime = timeSince(date);
-setInterval(function () {
-  document.getElementsByClassName("update")[0].innerHTML = timeSince(date);
-}, 30000);
-document.getElementsByClassName("update")[0].innerHTML = timeSince(date);
+function time_ago(time) {
+  switch (typeof time) {
+    case "number":
+      break;
+    case "string":
+      time = +new Date(time);
+      break;
+    case "object":
+      if (time.constructor === Date) time = time.getTime();
+      break;
+    default:
+      time = +new Date();
+  }
+  var time_formats = [
+    [60, "seconds", 1], // 60
+    [120, "1 minute ago", "1 minute from now"], // 60*2
+    [3600, "minutes", 60], // 60*60, 60
+    [7200, "1 hour ago", "1 hour from now"], // 60*60*2
+    [86400, "hours", 3600], // 60*60*24, 60*60
+    [172800, "Yesterday", "Tomorrow"], // 60*60*24*2
+    [604800, "days", 86400], // 60*60*24*7, 60*60*24
+    [1209600, "Last week", "Next week"], // 60*60*24*7*4*2
+    [2419200, "weeks", 604800], // 60*60*24*7*4, 60*60*24*7
+    [4838400, "Last month", "Next month"], // 60*60*24*7*4*2
+    [29030400, "months", 2419200], // 60*60*24*7*4*12, 60*60*24*7*4
+    [58060800, "Last year", "Next year"], // 60*60*24*7*4*12*2
+    [2903040000, "years", 29030400], // 60*60*24*7*4*12*100, 60*60*24*7*4*12
+    [5806080000, "Last century", "Next century"], // 60*60*24*7*4*12*100*2
+    [58060800000, "centuries", 2903040000], // 60*60*24*7*4*12*100*20, 60*60*24*7*4*12*100
+  ];
+  var seconds = (+new Date() - time) / 1000,
+    token = "ago",
+    list_choice = 1;
 
-function timeSince(date) {
-  var seconds = Math.floor((new Date() - date) / 1000);
-  var interval = Math.floor(seconds / 31536000);
-
-  if (interval > 1) {
-    return interval + " years";
+  if (seconds == 0) {
+    return "Just now";
   }
-  interval = Math.floor(seconds / 2592000);
-  if (interval > 1) {
-    return interval + " months";
+  if (seconds < 0) {
+    seconds = Math.abs(seconds);
+    token = "from now";
+    list_choice = 2;
   }
-  interval = Math.floor(seconds / 86400);
-  if (interval > 1) {
-    return interval + " days";
-  }
-  interval = Math.floor(seconds / 3600);
-  if (interval > 1) {
-    return interval + " hours";
-  }
-  interval = Math.floor(seconds / 60);
-  if (interval >= 1) {
-    return interval + " minutes";
-  }
-  return Math.floor(seconds) + " seconds";
+  var i = 0,
+    format;
+  while ((format = time_formats[i++]))
+    if (seconds < format[0]) {
+      if (typeof format[2] == "string") return format[list_choice];
+      else return Math.floor(seconds / format[2]) + " " + format[1] + " " + token;
+    }
+  return time;
 }
+
+var aDay = 24 * 60 * 60 * 1000;
+
+const textKpi = time_ago("2023-07-07T23:30:00");
+document.querySelector(".update").innerHTML = textKpi;
 
 var kpiJuni = document.getElementById("kpijuni");
 new Chart("kpijuni", {
